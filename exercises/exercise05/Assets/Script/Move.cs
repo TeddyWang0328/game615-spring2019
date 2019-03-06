279f55a481af679d7a9bde5a82970159f94b0b10
TeddyWang0328 <Wangte328@gmail.com>
2019-03-05 23:02:06 -0500

    exercise05

---
+++ exercises/exercise05/Assets/Script/Move.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 2; //[1] object moving speed
+    public Transform[] target;  // [2]  target
+    public float delta = 0.2f; //error value
+    private static int i = 0;
+
+
+    void Update()
+    {
+        moveTo();
+
+
+    }
+
+    void moveTo()
+    {
+
+        target[i].position = new Vector3(target[i].position.x, transform.position.y, target[i].position.z);
+
+        // [4] Bring the object towards the target point 
+        transform.LookAt(target[i]);
+
+        // [5] The object moves forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // [6] Determine whether the object has reached the target point
+        if (transform.position.x > target[i].position.x - delta
+            && transform.position.x < target[i].position.x + delta
+            && transform.position.z > target[i].position.z - delta
+            && transform.position.z < target[i].position.z + delta)
+            i = (i + 1) % target.Length;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Chicken")
+        {
+            Debug.Log("Destory" + other.gameObject.name);
+            Destroy(other.gameObject);
+
+
+        }
+
+
+    }
+}
---
+++ exercises/exercise05/Assets/Script/PlaneController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneController : MonoBehaviour
+{
+
+    public float forwardSpeed = 15f;
+    public float rollSpeed = 35f;
+    public float pitchSpeed = 20f;
+
+    public float pitchSpeedRate = 5f;
+
+    float cameraFollowBehindAmount = 15;
+    float cameraFollowAboveAmount = 5;
+
+    public GameObject shitPrefab;
+    public float shitLaunchForce = 1000f;
+
+
+    Rigidbody rigidbody;
+
+   
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the up/down/left/right input values
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        //Rotate
+        transform.Rotate(vAxis * rollSpeed * Time.deltaTime, hAxis * pitchSpeed / 4 * Time.deltaTime, -hAxis * pitchSpeed * Time.deltaTime, Space.Self);
+
+        void OnTriggerEnter(Collider other)
+        {
+
+            if (other.gameObject.CompareTag("Pick Up"))
+            {
+
+                other.gameObject.SetActive(false);
+
+
+                
+            }
+
+
+        }
+
+        
+
+        //Move the plane and keep it above the terrain height
+        transform.Translate(transform.forward * forwardSpeed * Time.deltaTime, Space.World);
+        float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position);
+        if (transform.position.y < terrainHeight)
+        {
+            transform.position = new Vector3(transform.position.x, terrainHeight, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject football = Instantiate(shitPrefab, transform.position + transform.forward * 5, transform.rotation);
+            Rigidbody footballRB = football.GetComponent<Rigidbody>();
+            footballRB.AddForce(transform.forward * shitLaunchForce);
+        }
+
+        //Position the camera
+        Camera.main.transform.position = transform.position + -transform.forward * cameraFollowBehindAmount + Vector3.up * cameraFollowAboveAmount;
+        Camera.main.transform.LookAt(transform);
+    }
+}
