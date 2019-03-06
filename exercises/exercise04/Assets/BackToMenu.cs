99fe3049fc8c18ca5e33d8afd17cfb6174a163d7
TeddyWang0328 <Wangte328@gmail.com>
2019-03-05 23:09:47 -0500

    exercises04

---
+++ exercises/exercise04/Assets/BackToMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackToMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+
+
+        GameObject btnObj = GameObject.Find("Button");
+
+        Button btn = btnObj.GetComponent<Button>();
+
+        btn.onClick.AddListener(delegate ()
+
+        {
+
+            this.GoNextScene(btnObj);
+
+        });
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+    }
+
+
+
+    public void GoNextScene(GameObject NScene)
+
+    {
+
+        Application.LoadLevel("GameStart");
+
+    }
+
+}
---
+++ exercises/exercise04/Assets/shell.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "enemy")
+        {
+            Debug.Log("Destory" + other.gameObject.name);
+            Destroy(other.gameObject);
+
+
+        }
+
+
+    }
+}
