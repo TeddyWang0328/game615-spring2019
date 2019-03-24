8d21b72241be79c73c59295e3aae9e3df5c3974b
TeddyWang0328 <Wangte328@gmail.com>
2019-03-24 15:47:31 -0400

    mid

---
+++ exercises/midterm/Assets/Scripts/biu.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class biu : MonoBehaviour
+{
+
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+
+
+    
+    private int count;
+
+
+    void Start()
+    {
+
+        
+
+        count = 0;
+
+
+        SetCountText();
+
+
+        winText.text = "";
+
+    }
+
+
+    void FixedUpdate()
+    {
+
+        
+
+       
+
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+
+            other.gameObject.SetActive(false);
+
+
+            count = count + 1;
+
+
+            SetCountText();
+        }
+
+
+    }
+
+
+    void SetCountText()
+    {
+
+        countText.text = "Target: " + count.ToString();
+
+
+        if (count >= 12)
+        {
+
+            winText.text = "Please click the Hint button to see the key information";
+            SceneManager.LoadScene("st1");
+        }
+    }
+}
